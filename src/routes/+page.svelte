4960acef70b62bc8ae4ff9ee16cd3e3b5373b223
Hamid <100763844+hamidettajani@users.noreply.github.com>
2023-09-27 22:15:25 +0200

    La til global color-scheme og font-families

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <div class="bg-purple-700 py-8 w-full">
     <div class="container mx-auto flex justify-center">
-        <p class="text-white text-4xl font-bold">Hamids Laboratorium</p>
+        <p class="text-white text-6xl font-bold">Hamids Laboratorium</p>
     </div>
   </div>
-  <div class="align-items-center">
-      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/7wnowNPtuurRlcTbqRRZd9?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1LB0DUOYXEndEQcgsPkCnR?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/7Hp0lNFiuf9hbFTy4FnUJM?utm_source=generator&theme=0" width="20%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/33ihooP7GA6a6yofsPGPLO?utm_source=generator" width="19%%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/0y6NrkEQdihg957W0q6C78?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    </div>
+  <h1 class="text-5xl font-bold absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">FAVORITT MUSIKK</h1>
+  <div class="flex gap-4 justify-center mt-56">
+    <iframe class="" style="border-radius:15px" src="https://open.spotify.com/embed/playlist/7wnowNPtuurRlcTbqRRZd9?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <iframe class="" style="border-radius:15px" src="https://open.spotify.com/embed/playlist/1LB0DUOYXEndEQcgsPkCnR?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <iframe class="" style="border-radius:15px" src="https://open.spotify.com/embed/playlist/7Hp0lNFiuf9hbFTy4FnUJM?utm_source=generator&theme=0" width="20%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <iframe class="" style="border-radius:15px" src="https://open.spotify.com/embed/playlist/33ihooP7GA6a6yofsPGPLO?utm_source=generator" width="19%%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <iframe class="" style="border-radius:15px" src="https://open.spotify.com/embed/playlist/0y6NrkEQdihg957W0q6C78?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+  </div>
