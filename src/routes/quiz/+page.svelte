8e1f426c8d0ac328d9e13edfb4d92e8f30081d52
Hamid <100763844+hamidettajani@users.noreply.github.com>
2023-09-28 10:32:42 +0200

    La til interaktiv quiz

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -78,15 +78,3 @@
       </div>
     {/if}
   </main>
-  
-  <!--Favoritt musikk display-->
-  <h1 class="text-5xl font-bold absolute top-60 left-1/2 transform -translate-x-1/2 -translate-y-1/2">FAVORITT MUSIKK</h1>
-  <div class="flex gap-4 justify-center mt-56">
-    <iframe class="" style="border-radius:15px" src="https://open.spotify.com/embed/playlist/7wnowNPtuurRlcTbqRRZd9?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    <iframe class="" style="border-radius:15px" src="https://open.spotify.com/embed/playlist/1LB0DUOYXEndEQcgsPkCnR?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    <iframe class="" style="border-radius:15px" src="https://open.spotify.com/embed/playlist/7Hp0lNFiuf9hbFTy4FnUJM?utm_source=generator&theme=0" width="20%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    <iframe class="" style="border-radius:15px" src="https://open.spotify.com/embed/playlist/33ihooP7GA6a6yofsPGPLO?utm_source=generator" width="19%%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    <iframe class="" style="border-radius:15px" src="https://open.spotify.com/embed/playlist/0y6NrkEQdihg957W0q6C78?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-  </div>
-
-<p></p>
