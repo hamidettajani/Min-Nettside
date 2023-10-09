73f4c227bf323e57d85d30ac592cf19885b42a9b
Hamid <100763844+hamidettajani@users.noreply.github.com>
2023-10-09 13:47:03 +0200

    bugfix
    nå fungerer tailwindcss på alle sider

--- src/routes/musikk/+page.svelte
+++ src/routes/musikk/+page.svelte
@@ -10,11 +10,15 @@
     </div>
   </div>
   <!--Favoritt musikk display-->
-  <h1 class="text-5xl font-bold absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">FAVORITT MUSIKK</h1>
-  <div class="flex gap-4 justify-center mt-56">
-    <iframe class="" style="border-radius:15px" title="Playlist" src="https://open.spotify.com/embed/playlist/7wnowNPtuurRlcTbqRRZd9?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    <iframe class="" style="border-radius:15px" title="Playlist" src="https://open.spotify.com/embed/playlist/1LB0DUOYXEndEQcgsPkCnR?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    <iframe class="" style="border-radius:15px" title="Playlist" src="https://open.spotify.com/embed/playlist/7Hp0lNFiuf9hbFTy4FnUJM?utm_source=generator&theme=0" width="20%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    <iframe class="" style="border-radius:15px" title="Playlist" src="https://open.spotify.com/embed/playlist/33ihooP7GA6a6yofsPGPLO?utm_source=generator" width="19%%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    <iframe class="" style="border-radius:15px" title="Playlist" src="https://open.spotify.com/embed/playlist/0y6NrkEQdihg957W0q6C78?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+  <div class="">
+    <div class="text-5xl mb-4 font-bold flex items-center justify-center">
+      FAVORITT MUSIKK
+    </div>
+    <div class="flex gap-4 justify-center">
+      <iframe class="" style="border-radius:15px" title="Playlist" src="https://open.spotify.com/embed/playlist/7wnowNPtuurRlcTbqRRZd9?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      <iframe class="" style="border-radius:15px" title="Playlist" src="https://open.spotify.com/embed/playlist/1LB0DUOYXEndEQcgsPkCnR?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      <iframe class="" style="border-radius:15px" title="Playlist" src="https://open.spotify.com/embed/playlist/7Hp0lNFiuf9hbFTy4FnUJM?utm_source=generator&theme=0" width="20%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      <iframe class="" style="border-radius:15px" title="Playlist" src="https://open.spotify.com/embed/playlist/33ihooP7GA6a6yofsPGPLO?utm_source=generator" width="19%%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      <iframe class="" style="border-radius:15px" title="Playlist" src="https://open.spotify.com/embed/playlist/0y6NrkEQdihg957W0q6C78?utm_source=generator" width="19%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
   </div>
